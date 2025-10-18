f1dbc152e5b50b35d54487d00dc0cb5929070034
Denis Safiullin <fckninja@gmail.com>
2025-10-18 23:40:55 +0800

    add basic authentication

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-import { Hono } from "hono";
+import { Hono } from 'hono';
+import { basicAuth } from 'hono/basic-auth'
+import { logger } from 'hono/logger'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-app.get("/message", (c) => {
-  return c.text("Hello Hono!");
+app.use(logger())
+app.use('/message', basicAuth({
+  verifyUser: (username, password, c) => {
+    return (
+      username === c.env.USERNAME
+      && password === c.env.PASSWORD
+    )
+  },
+}))
+
+app.get("/message", async (c) => {
+  const llm_token = c.env.LLM_TOKEN
+  console.log(llm_token)
+  return c.text('Hello, user!')
 });
 
 export default app;
